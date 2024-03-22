3590cc67455855a6383481cb58aa579b9d3a1bfc
bhe2bhe <bhe2bhe@gmail.com>
2024-03-22 21:07:03 +0800

    back to no corshearder

--- chikaness/settings.py
+++ chikaness/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,12 +55,6 @@ MIDDLEWARE = [
 
 if not DEBUG:
     CSRF_TRUSTED_ORIGINS = [
-        'https://*.railway.app',
-        'https://web-production-d21e.up.railway.app',
-        'https://chikaness.com',
-    ]
-
-    CORS_ALLOWED_ORIGINS = [
         'https://*.railway.app',
         'https://web-production-d21e.up.railway.app',
         'https://chikaness.com',
