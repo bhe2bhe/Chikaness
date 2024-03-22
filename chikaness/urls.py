0dbf5026d9e210c8eae367122ac253abd62c9380
bhe2bhe <bhe2bhe@gmail.com>
2024-03-23 01:53:56 +0800

    changes in urls.py

--- chikaness/urls.py
+++ chikaness/urls.py
@@ -16,14 +16,18 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
+
+from django.views.static import serve
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    re_path(r'^pictures/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('',include('users.urls')),
     path('blogs/',include('blogs.urls')),
     path('admin/', admin.site.urls),
