0d19e198162534eb476ddf22db9f079a991659ca
bhe2bhe <bhe2bhe@gmail.com>
2024-03-22 21:29:29 +0800

    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]

--- chikaness/settings.py
+++ chikaness/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uqdwds#xkqs+v#9#4h^a5ng&2ey-=^9shr0=s8vue4lc_vi2up'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['chikaness.com', 'web-production-d21e.up.railway.app', '127.0.0.1', 'localhost']
 
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
